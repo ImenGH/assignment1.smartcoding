6fbf3c9e21552394aea1d911de7fa9648779fae1
Imen Grab <ghrabimene@gmail.com>
2018-11-29 15:51:04 +0100

    solution of question_3.py

--- question_3.py
+++ question_3.py
@@ -1,15 +1,26 @@
 
 def getChange(cost, amount):
-    cost = input("Enter the cost: ")
-    amount = input("Enter the amount: ")
-    change = float(amount) - float(cost)
-    print("the change is: ", float(change))
-    if change >= 10:
-        get_ten = change / 10
-    print("the number of coin of ten sek is : ", int(get_ten))
+    change = amount - cost
+    print("the change is: ", change)
+    coins_of_10 = int(change / 10)
+    print("the number of coin of ten sek is : ", coins_of_10)
+    change = change - (10 * coins_of_10)
+    if change >= 0:
+        coins_of_5 =  int(change / 5)
+        print("the number of coin of five sek is : ", coins_of_5)
+        change =  change - (5 * coins_of_5)
+        if change >= 0:
+            coins_of_2 =  int(change / 2)
+            print("the number of coin of two sek is : ", coins_of_2)
+            change =  change - (2 * coins_of_2)
+            if change >= 0:
+                coins_of_1 = int(change / 1)
+            print("the number of coin of one sek is : ", coins_of_1)
+            change = change - (1 * coins_of_1)
 
-    #get_five = (change - (get_ten * 10)) / 5
-    #get_two = (change - (get_ten * 10) - (get_five * 5)) / 2
-    #get_one = (change) - (get_ten *10) - (get_five *5) -(get_two *2) / 1)
 
-getChange(cost, amount)
+
+getChange(10, 100)
+getChange(15, 50)
+getChange(13, 50)
+getChange(5, 8)
